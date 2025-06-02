671bed59c5d88ed4c66e13482dc8a679a8fbcd5e
ADUDESLAPTOP\ncta2 <nick.nguyen2150@gmail.com>
2025-06-02 11:31:22 +1000

    update project panel - added data processing portal

--- src/components/project-display.js
+++ src/components/project-display.js
@@ -10,14 +10,15 @@ import Portfolio from '../resources/Portfolio.png';
 import AIA from '../resources/AIA.png';
 import BioNix from '../resources/BioNix.png';
 import Kamo from '../resources/kamo.png';
+import DataProcessingPortal from '../resources/DataProcessingPortal.png';
 import formatContent from '../functions/util';
 
 function ProjectDisplay() {
     const [slide, setSlide] = useState(0);
-    const projects = [['Algorithms in Action', 'A website that visualizes different computer algorithms for students to understand them better and educators to better deliver their lessons', 'Tech: Javascript, CSS, HTML'], ['BioNix', 'Wrapping bioinformatics software into a package manager called BioNix', 'Tech: Nix, Markdown'], ['Unrefusable Valentine', 'An ultimate web app designed to ask someone to be your Valentine in a way that is impossible to say no', 'Tech: ReactJS, TailwindCSS, HTML'], ['Portfolio', 'My digital portofio to showcase my experiences, projects, contacts and most importantly, my UI design and development capabilities', 'Tech: ReactJS, HTML, CSS'], ['Kamo', 'An AI Chatbot that assists students with subjects information of multiple universities, and course optimization', 'Tech: VueJS, CSS, HTML, .Net Core, MySQL']] 
-    const githubUrls = ['https://github.com/tuankhainguy/algorithms-in-action.github.io', 'https://github.com/WEHI-ResearchComputing/BioNix-GettingStarted/wiki', 'https://github.com/nicked1205/Unrefusable-Valentine', 'https://github.com/nicked1205/Portfolio', '']
-    const webUrls = ['https://dev-aia.vercel.app/', '', 'https://valentine-delta-livid.vercel.app/', 'https://portfolio-mu-ecru-53.vercel.app/', '']
-    const imageUrls = [AIA, BioNix, UnrefusableValentine, Portfolio, Kamo]
+    const projects = [['Algorithms in Action', 'A website that visualizes different computer algorithms for students to understand them better and educators to better deliver their lessons', 'Tech: Javascript, CSS, HTML'], ['BioNix', 'Wrapping bioinformatics software into a package manager called BioNix', 'Tech: Nix, Markdown'], ['Unrefusable Valentine', 'An ultimate web app designed to ask someone to be your Valentine in a way that is impossible to say no', 'Tech: ReactJS, TailwindCSS, HTML'], ['Portfolio', 'My digital portofio to showcase my experiences, projects, contacts and most importantly, my UI design and development capabilities', 'Tech: ReactJS, HTML, CSS'], ['Kamo', 'An AI Chatbot that assists students with subjects information of multiple universities, and course optimization', 'Tech: VueJS, CSS, HTML, .Net Core, MySQL'], ['Data Processing Portal', 'A website that receives a list of product information page URLs of any eCommerce websites in Australia, and a custom prompt denoting the curation of data, scrapes the data and returns downloadable CSV or Excel files with the curated data', 'Tech: ReactJS, Express, TailwindCSS, OpenAI API']] 
+    const githubUrls = ['https://github.com/tuankhainguy/algorithms-in-action.github.io', 'https://github.com/WEHI-ResearchComputing/BioNix-GettingStarted/wiki', 'https://github.com/nicked1205/Unrefusable-Valentine', 'https://github.com/nicked1205/Portfolio', '', 'https://github.com/nicked1205/Data-Processing-Portal']
+    const webUrls = ['https://dev-aia.vercel.app/', '', 'https://valentine-delta-livid.vercel.app/', 'https://portfolio-mu-ecru-53.vercel.app/', '', 'https://data-processing-portal-blush.vercel.app/']
+    const imageUrls = [AIA, BioNix, UnrefusableValentine, Portfolio, Kamo, DataProcessingPortal]
     const [formattedContent, setFormattedContent] = useState()
 
     useEffect(() => {
