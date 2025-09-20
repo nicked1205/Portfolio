5f51f2c8d0aa4b06a9d26c6511a4c29707738a5a
ADUDESLAPTOP\ncta2 <nick.nguyen2150@gmail.com>
2025-09-20 18:16:18 +0930

    general info update

--- src/components/information-display.js
+++ src/components/information-display.js
@@ -4,7 +4,7 @@ import formatContent from '../functions/util';
 function InformationDisplay() {
     let title ="Introduction";
     let contents = ["Name: Cat Tuong Anh Nguyen","Age: 21",
-        "Description: Working on the Bachelor of Science degree from the University of Melbourne, and developing Computing and Software Systems knowledge through internships and personal projects.",
+        "Description: Developing Computing and Software Systems, and AIML knowledge through internships and personal projects.",
         "Take pride on positive respond to criticism, will to learn, to become better despite what to come, and ability to adapt.", 
         "Life motto: “It’s better to light a candle than to curse the darkness.” - By William Lonsdale Atkinson."];
 
